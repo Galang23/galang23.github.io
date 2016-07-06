43415649bcf12ecb6d3ae381803bebd8c8827016
Galang23 <wicaksono_ag@outlook.co.id>
2016-07-06 17:03:56 +0700

    Sesuatu baru; Mohon maaf lahir batin!

--- push.sh
+++ push.sh
@@ -2,12 +2,14 @@
 
 #
 # This is shell script for pushing to github
+# Works on Windows git-bash and UNIX system. also GNU
 #
 
 
 git add . -v
 echo -n "Commit message: "
 read MESSAGE
+# artinya read input user sebagai variabel MESSAGE
 git commit -m "$MESSAGE"
 echo "Pushing change to Galang23 @ branch master..."
 git push Galang23 master
