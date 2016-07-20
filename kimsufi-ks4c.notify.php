59fc1283e259971b10b97f36b0a9cc964b3608c1
Galang23 <wicaksono_ag@outlook.co.id>
2016-07-20 12:37:48 +0700

    Renew TMS

--- kimsufi-ks4c.notify.php
+++ kimsufi-ks4c.notify.php
@@ -11,14 +11,14 @@ return file_get_contents($address,0,$context);
 }
 while(true){
 	$content = httpget('https://www.kimsufi.com/en/order/kimsufi.cgi?hard=162sk42',10);
-	$content2 = httpget("https://127.0.0.1",10);
+	//$content2 = httpget("https://127.0.0.1",10);
 	if($content){
 		if(strpos($content,'est invalide')==false){
-			api_notify("[kimsufi]Appear! ","Time: " . date("Y-m-d-H:i",time()+46800),1,2);//Here should go your notify function
+			api_notify("[kimsufi] Appear! ","Time: " . date("Y-m-d-H:i",time()+46800),1,2);//Here should go your notify function
 		}
 	}else{
 		if(strpos($content2,'est invalide') == true){
-			api_notify("[kimsufi]Doesn't appear. Sorry :(! ", "Time: " . date("Y-m-d-H:i",time()+46800),1,2);//Here should go your notify function
+			api_notify("[kimsufi] Doesn't appear. Sorry :(! ", "Time: " . date("Y-m-d-H:i",time()+46800),1,2);//Here should go your notify function
 		}
 	}
 sleep(60);
