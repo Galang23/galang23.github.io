c9e4560a04defbec41a72626e372769f8db3fd50
Galang23 <wicaksono_ag@outlook.co.id>
2017-01-22 20:30:42 +0700

    nsfw viewer 2.0, new english, refresh home, fix grammar, refresh css

--- js/view9cdn.js
+++ js/view9cdn.js
@@ -1,41 +1,3 @@
-var responseHTML = document.createElement("body");
-
-function getBody(content) 
-{
-   test = content.toLowerCase();    // to eliminate case sensitivity
-   var x = test.indexOf("<body");
-   if(x == -1) return "";
-
-   x = test.indexOf(">", x);
-   if(x == -1) return "";
-
-   var y = test.lastIndexOf("</body>");
-   if(y == -1) y = test.lastIndexOf("</html>");
-   if(y == -1) y = content.length;    // If no HTML then just grab everything till end
-
-   return content.slice(x + 1, y);   
-} 
-
-function loadHTML(url, fun, storage, param)
-{
-	var xhr = createXHR();
-	xhr.onreadystatechange=function()
-	{ 
-		if(xhr.readyState == 4)
-		{
-			//if(xhr.status == 200)
-			{
-				storage.innerHTML = getBody(xhr.responseText);
-				fun(storage, param);
-			}
-		} 
-	}; 
-
-	xhr.open("GET", url , true);
-	xhr.send(null); 
-
-} 
-
 function view(){
 	var img = document.getElementById("9gagID").value;
 	var image = "http://img-9gag-fun.9cache.com/photo/" + img + "_700b.jpg";
@@ -44,4 +6,16 @@ function view(){
 	document.getElementById("result").src = image;
 	// document.getElementById("gif").src = gif; //GIF is deprecated. MP4 used instead.
 	document.getElementById("vid").src = vid;
+}
+
+function getQuery(field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+}
+
+function setPostID(){
+	var PostID = getQuery("postID");
+	
 }
