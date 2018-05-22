3a0ce63a847d860f8dda1ee484a46ebe5f84b446
Galang23 <wicaksono_ag@outlook.co.id>
2018-05-22 09:56:46 +0700

    Minor update pada blog dan removal some unuseful file

--- js/view9cdn.js
+++ js/view9cdn.js
@@ -1,11 +1,16 @@
 function view(){
 	var img = document.getElementById("9gagID").value;
-	var image = "http://img-9gag-fun.9cache.com/photo/" + img + "_700b.jpg";
-	//var gif =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sa.gif"; //gif is  deprecated. MP4 used instead.
-	var vid =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sv.mp4";
-	document.getElementById("result").src = image;
-	// document.getElementById("gif").src = gif; //GIF is deprecated. MP4 used instead.
-	document.getElementById("vid").src = vid;
+	if(img.length  === 0) {
+					document.getElementById("result").src = "https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png";
+	                //document.getElementById("result").src = "http://i0.kym-cdn.com/photos/images/original/001/157/196/bcf.gif";
+	} else {
+		var image = "http://img-9gag-fun.9cache.com/photo/" + img + "_700b.jpg";
+		//var gif =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sa.gif"; //gif is  deprecated. MP4 used instead.
+		var vid =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sv.mp4";
+		document.getElementById("result").src = image;
+		// document.getElementById("gif").src = gif; //GIF is deprecated. MP4 used instead.
+		document.getElementById("vid").src = vid;
+	}
 }
 
 function getQuery(field, url) {
@@ -17,5 +22,4 @@ function getQuery(field, url) {
 
 function setPostID(){
 	var PostID = getQuery("postID");
-	
 }
