b542c286b81d46407d32b4b304dfe053a603650f
Suwarna Adhi Galang Wicaksono <Galang23@users.noreply.github.com>
2018-09-30 23:21:32 +0700

    Fix for video not being shown by changing url
    Use webm to adapt with new 9gag

--- js/view9cdn.js
+++ js/view9cdn.js
@@ -1,14 +1,13 @@
 function view(){
 	var img = document.getElementById("9gagID").value;
 	if(img.length  === 0) {
-					document.getElementById("result").src = "https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png";
-	                //document.getElementById("result").src = "http://i0.kym-cdn.com/photos/images/original/001/157/196/bcf.gif";
+		document.getElementById("result").src = "https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png";
 	} else {
-		var image = "http://img-9gag-fun.9cache.com/photo/" + img + "_700b.jpg";
+		var image = "https://img-9gag-fun.9cache.com/photo/" + img + "_700b.jpg";
 		//var gif =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sa.gif"; //gif is  deprecated. MP4 used instead.
-		var vid =  "http://img-9gag-fun.9cache.com/photo/" + img + "_460sv.mp4";
+		var vid =  "https://img-9gag-fun.9cache.com/photo/" + img + "_460svvp9.webm";
 		document.getElementById("result").src = image;
-		// document.getElementById("gif").src = gif; //GIF is deprecated. MP4 used instead.
+		// document.getElementById("gif").src = gif; //GIF is deprecated. MP4 is not working. Use webm VP9.
 		document.getElementById("vid").src = vid;
 	}
 }
